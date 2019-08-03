d211992468acfde2c910aae1f8cbb5713b79515a
ISMAIL RAMZAN <m-ismail-ramzan@yahoo.com>
2019-08-03 20:42:30 +0500

    Updates

---
+++ precache-manifest.ca28d7154d7315e04bd19af312ac47e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "946b77b52e3f2633ff4a5948a6bc39d1",
+    "url": "/tossgame/index.html"
+  },
+  {
+    "revision": "b9c8c9b31de828f4894c",
+    "url": "/tossgame/static/css/main.f9b02786.chunk.css"
+  },
+  {
+    "revision": "4560886a3cb028e7a3be",
+    "url": "/tossgame/static/js/2.8a586615.chunk.js"
+  },
+  {
+    "revision": "b9c8c9b31de828f4894c",
+    "url": "/tossgame/static/js/main.d225bee2.chunk.js"
+  },
+  {
+    "revision": "b534b31605ebc05740d5",
+    "url": "/tossgame/static/js/runtime~main.842ac6b5.js"
+  }
+]);
